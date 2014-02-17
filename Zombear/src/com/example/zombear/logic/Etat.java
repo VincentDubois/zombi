d53d977374daa89fcc2323a2d9e72807f668c397
Furnari <adrien.furnari@103f03.iut-lens.univ-artois.fr>
2014-02-17 16:52:32 +0100

    Sauvegarde rotation tablette, actualisation jauge/action

--- Zombear/src/com/example/zombear/logic/Etat.java
+++ Zombear/src/com/example/zombear/logic/Etat.java
@@ -2,6 +2,7 @@ package com.example.zombear.logic;
 
 import java.util.Date;
 
+import android.os.Bundle;
 import android.util.Log;
 
 public class Etat {
@@ -12,26 +13,31 @@ public class Etat {
 
 	public Jauge bonheur;
 
-	private Jauge zombification;
+	public Jauge zombification;
 	
-	private Boolean malade;
-	private Boolean endormi;
+	public Boolean malade = false;
+	public Boolean endormi = false;
 	
-	private long lastUpdate;
+	public long lastUpdate;
 	
 	public Etat(){
 		Log.d("Zomb", "Initialisation");
-		initialiser();
-		Log.d("Zomb", "Update");
-		update();
-	}
-	
-	public void initialiser(){
 		faim = new Jauge();
 		sommeil = new Jauge();
 		bonheur = new Jauge();
 		zombification = new Jauge();
 		lastUpdate = new Date().getTime();
+		Log.d("Zomb", "Update");
+		update();
+	}
+	
+	public Etat(Bundle b){
+		Log.d("Zomb", "Initialisation via un Bundle");
+		faim = new Jauge(b.getDouble("faim"));
+		sommeil= new Jauge(b.getDouble("sommeil"));
+		bonheur = new Jauge();
+		zombification = new Jauge();
+		lastUpdate = b.getLong("lastUpdate");
 	}
 	
 	public int getNiveauJauge(Jauge j){
@@ -46,13 +52,21 @@ public class Etat {
 		j.diminuerJauge(v);
 	}
 	
+	public Bundle getSaveBundle(){
+		Bundle b = new Bundle();
+		b.putDouble("faim", faim.getNiveau());
+		b.putDouble("sommeil", sommeil.getNiveau());
+		b.putLong("lastUpdate", lastUpdate);
+		return b;
+	}
+	
 	public void update(){
-		Log.d("update", faim.getNiveau()+" "+sommeil.getNiveau());
 		long time = new Date().getTime();
 		long tempsEcoule = time - lastUpdate;
-		diminuerNiveauJauge(faim, lastUpdate/60000);
-		diminuerNiveauJauge(sommeil, lastUpdate/120000);
-		
+		diminuerNiveauJauge(faim, (double)tempsEcoule/6000);
+		if(endormi) augmenterNiveauJauge(sommeil, (double)tempsEcoule/8000);
+		else diminuerNiveauJauge(sommeil, (double)tempsEcoule/12000);
+		Log.d("update", "Faim : "+faim+" | Sommeil : "+sommeil);
 		lastUpdate = time;
 	}
 
