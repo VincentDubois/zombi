c4c86a6cc71edee9bc4650b74bcbd5d0ed808b81
MaximeDefenin <kaelric@hotmail.fr>
2014-04-03 12:53:38 +0200

    Ajout fonction getItemByAction

--- Zombear/src/fr/univartois/iutlens/zombfox/view/Gamelle.java
+++ Zombear/src/fr/univartois/iutlens/zombfox/view/Gamelle.java
@@ -4,12 +4,13 @@ import android.graphics.PointF;
 import fr.univartois.iutlens.zombfox.utils.SpriteSheet;
 
 public class Gamelle extends Item{
-	public Gamelle(boolean dispo, int etat, int position_sprite,
+
+	public Gamelle(boolean dispo, int etat, int position_sprite, String action,
 			SpriteSheet sprite, PointF posS) {
-		super(dispo, etat, position_sprite, sprite, posS);
+		super(dispo, etat, position_sprite, action, sprite, posS);
 		// TODO Auto-generated constructor stub
-		
 	}
+	
 
 
 }
